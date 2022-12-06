cd40646c54bb59094855f1aae650d0de60ef5939
Kabir S. Tamari <kabirtamari@gmail.com>
2022-12-06 05:23:24 +0000

    work in progress

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+
+(function ($) {
+
+  "use strict";
+
+  // AOS ANIMATIONS
+  AOS.init();
+
+  // NAVBAR
+  $('.navbar-nav .nav-link').click(function () {
+    $(".navbar-collapse").collapse('hide');
+  });
+
+
+  $('a[href*="#"]').click(function (event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 66
+        }, 1000);
+      }
+    }
+  });
+
+})(window.jQuery);
